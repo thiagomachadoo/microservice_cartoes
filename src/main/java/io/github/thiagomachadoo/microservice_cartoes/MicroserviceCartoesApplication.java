7ee443e53543ce8bb8851be24cf58f44924a13f4
Thiago da Silva Machado <thiagodasilvamachadoo44@gmail.com>
2023-03-31 20:28:48 -0300

    Initial commit

---
+++ src/main/java/io/github/thiagomachadoo/microservice_cartoes/MicroserviceCartoesApplication.java
@@ -0,0 +1,13 @@
+package io.github.thiagomachadoo.microservice_cartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCartoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCartoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/thiagomachadoo/microservice_cartoes/MicroserviceCartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.thiagomachadoo.microservice_cartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCartoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
